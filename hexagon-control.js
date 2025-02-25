cdf01c591767139eaad28aa8c7fda7ddbb2d7550
Hamitouche Yacine <44175867+youlou007@users.noreply.github.com>
2025-02-25 21:18:10 +0100

    Delete hexagon-control.js

--- hexagon-control.js
+++
@@ -1,7 +0,0 @@
-function setHexagonGap(pixels) {
-    document.documentElement.style.setProperty('--hex-gap-horizontal', pixels + 'px');
-}
-
-// Exemple d'utilisation :
-// setHexagonGap(15); // Pour définir un écart de 15 pixels
-// setHexagonGap(20); // Pour définir un écart de 20 pixels
